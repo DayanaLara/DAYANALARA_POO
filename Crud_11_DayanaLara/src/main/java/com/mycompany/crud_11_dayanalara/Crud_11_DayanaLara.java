a6f9d2253eb62e318d9de385b008514062fd2918
DayanaLara <dayana.lara.j@vallegrande.edu.pe>
2024-06-23 23:18:52 -0500

    Agregar carpeta java

---
+++ Crud_11_DayanaLara/src/main/java/com/mycompany/crud_11_dayanalara/Crud_11_DayanaLara.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.crud_11_dayanalara;
+
+/**
+ *
+ * @author USER
+ */
+public class Crud_11_DayanaLara {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
